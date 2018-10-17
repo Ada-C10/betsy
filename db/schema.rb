7ded64d308226e5b26fa1b2a680a214090e4bd9f
Alice Hsiao <alice.hsiao87@gmail.com>
2018-10-17 16:09:46 -0700

    Added join table between merchants and orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_17_230521) do
+ActiveRecord::Schema.define(version: 2018_10_17_230742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2018_10_17_230521) do
     t.string "avatar_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "merchants_orders", force: :cascade do |t|
+    t.bigint "merchant_id"
+    t.bigint "order_id"
+    t.index ["merchant_id"], name: "index_merchants_orders_on_merchant_id"
+    t.index ["order_id"], name: "index_merchants_orders_on_order_id"
   end
 
   create_table "orderitems", force: :cascade do |t|
