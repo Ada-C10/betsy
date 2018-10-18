5202a83e7e051d36586b767ff613e4fc86aa4e16
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-17 21:29:04 -0700

    Updated all current fixture files

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -3,7 +3,53 @@ require "test_helper"
 describe Product do
   let(:product) { Product.new }
 
-  it "must be valid" do
-    value(product).must_be :valid?
+  describe "relations" do
+    it "has a list of votes" do
+       album = works(:album)
+       album.must_respond_to :votes
+       album.votes.each do |vote|
+         vote.must_be_kind_of Vote
+       end
+    end
+
+    it "has a list of voting users" do
+      album = works(:album)
+      album.must_respond_to :ranking_users
+      album.ranking_users.each do |user|
+        user.must_be_kind_of User
+      end
+    end
+  end
+
+  describe "relations" do
+    it "has a user" do
+      v = votes(:one)
+      v.must_respond_to :user
+      v.user.must_be_kind_of User
+    end
+
+    it "has a work" do
+      v = votes(:one)
+      v.must_respond_to :work
+      v.work.must_be_kind_of Work
+    end
+  end
+
+  describe "relations" do
+    it "has a list of votes" do
+      dan = users(:dan)
+      dan.must_respond_to :votes
+      dan.votes.each do |vote|
+        vote.must_be_kind_of Vote
+      end
+    end
+
+    it "has a list of ranked works" do
+      dan = users(:dan)
+      dan.must_respond_to :ranked_works
+      dan.ranked_works.each do |work|
+        work.must_be_kind_of Work
+      end
+    end
   end
 end
