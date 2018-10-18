abbc3bd5c1c6f9f793ff59c07d64ed1619e166dd
Alice Hsiao <alice.hsiao87@gmail.com>
2018-10-17 20:01:24 -0700

    Deleted category validation from product, added homepage route and all other routes

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,6 @@ class Product < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :cost, presence: true, numericality: { greater_than: 0 }, allow_nil: true
-  # validates :category, presence: true # let's think more about categories
   validates :inventory, presence: true, numericality: { only_integer: true }
   validates :description, presence: true
   validates :active, inclusion: { in: [true, false] }
