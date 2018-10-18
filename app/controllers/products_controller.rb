0edbde08246d549c409f238aa0496d1fbc080cf7
Alice Hsiao <alice.hsiao87@gmail.com>
2018-10-18 11:06:13 -0700

    Working on product new

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,9 +11,16 @@ class ProductsController < ApplicationController
   def show; end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
