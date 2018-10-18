4335041d3e7561648536ef6188c20f155e375f86
Shelan <shelandebesai@gmail.com>
2018-10-17 22:07:23 -0700

    validation test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -7,3 +7,20 @@ describe Product do
     value(product).must_be :valid?
   end
 end
+
+describe "validations" do
+  it "requires a name" do
+    product = Product.new
+    product.valid?.must_equal false
+    product.errors.messages.must_include :name
+  end
+end
+
+
+
+# validates :name, presence: true, uniqueness: true
+# validates :cost, presence: true, numericality: { greater_than: 0 }, allow_nil: true
+# validates :inventory, presence: true, numericality: { only_integer: true }
+# validates :description, presence: true
+# validates :active, inclusion: { in: [true, false] }
+# validates :image_url, presence: true
