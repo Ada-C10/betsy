92d2251830421ca86cc608e693c4a431d68a6254
Katie Jahanmir <katietj@gmail.com>
2018-10-18 14:41:38 -0700

    updated model relationships

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ApplicationRecord
   STATUSES = %w(pending paid complete cancelled)
   has_many :orderitems
   has_and_belongs_to_many :merchants
+  belongs_to :guests
 
   validates :status, presence: true, inclusion: { in: STATUSES }
   validates :name, presence: true
