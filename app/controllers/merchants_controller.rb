64f89ddb95adf77f7051a77cfcaaafcdbfef9a87
Pushpa Agr <pushpaagr108@gmail.com>
2018-10-18 15:38:20 -0700

    started def index..crud for merchant controllers

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,2 +1,25 @@
 class MerchantsController < ApplicationController
+
+  def index
+    @merchants = Merchant.all
+  end
+
+  def new
+    @merchant = Merchant.new
+  end
+
+  def create
+  end
+
+  def show
+    @merchant = Merchant.find_by(id: params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+
+
   resources :order_items
   resources :orders
   resources :categories
