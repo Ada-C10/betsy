b73e760d8e361d33ccd0541d42af4eb0221b8576
Alice Hsiao <alice.hsiao87@gmail.com>
2018-10-17 22:17:58 -0700

    Merchant Show page to see products

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -4,7 +4,9 @@ class MerchantsController < ApplicationController
     @merchants = Merchant.all.order(:name)
   end
 
-  def show; end
+  def show
+    @products = @merchant.products
+  end
 
   def new
   end
