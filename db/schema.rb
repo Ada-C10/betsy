3554b00b7db4c289502a73cd46f4b4be29ab6d4e
Pushpa Agr <pushpaagr108@gmail.com>
2018-10-18 14:44:50 -0700

    added order_id foreign key to the orderItem table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_18_213827) do
+ActiveRecord::Schema.define(version: 2018_10_18_214230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2018_10_18_213827) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "product_id"
+    t.bigint "order_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
     t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
@@ -70,6 +72,7 @@ ActiveRecord::Schema.define(version: 2018_10_18_213827) do
     t.index ["merchant_id"], name: "index_products_on_merchant_id"
   end
 
+  add_foreign_key "order_items", "orders"
   add_foreign_key "order_items", "products"
   add_foreign_key "products", "merchants"
 end
