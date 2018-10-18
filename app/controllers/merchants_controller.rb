056e8203cde3324a6814bda2f6fbc790fd219ec3
Alice Hsiao <alice.hsiao87@gmail.com>
2018-10-17 22:10:03 -0700

    Created basic navbar and views for product and merchant

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,9 +1,10 @@
 class MerchantsController < ApplicationController
+  before_action :find_merchant, only: [:show]
   def index
+    @merchants = Merchant.all.order(:name)
   end
 
-  def show
-  end
+  def show; end
 
   def new
   end
@@ -18,5 +19,17 @@ class MerchantsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def find_merchant
+    @merchant = Merchant.find_by(id: params[:id])
+
+    render_404 unless @merchant
+    #add flash messages + redirect
+  end
+
+  def merchant_params
+    params.require(:merchant).permit(:name, :email, :avatar_url, :uid, :provider)
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,14 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:show]
+
   def root
   end
 
   def index
+    @products = Product.all.order(:name)
   end
 
-  def show
-  end
+  def show; end
 
   def new
   end
@@ -21,5 +23,17 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def find_product
+    @product = Product.find_by(id: params[:id])
+
+    render_404 unless @product
+    #add flash messages + redirect
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :cost, :image_url, :inventory, :description, :active)
   end
 end
