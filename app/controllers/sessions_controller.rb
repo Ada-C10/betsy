310f7bf138cbc112c07b26a0f0dbd6f614c50461
Alice Hsiao <alice.hsiao87@gmail.com>
2018-10-19 05:45:02 -0700

    Worked on home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,24 @@
 class SessionsController < ApplicationController
   skip_before_action :require_login, only: [:create]
-  
+
   def create
     auth_hash = request.env['omniauth.auth']
 
     merchant = Merchant.find_by(uid: auth_hash[:uid], provider: 'github')
 
     if merchant
-      flash[:status] = :success
-      flash[:result_text] = "Logged in as returning user #{merchant.name}"
+      # flash[:status] = :success
+      # flash[:result_text] = "Logged in as returning user #{merchant.name}"
     else
       merchant = Merchant.build_from_github(auth_hash)
 
       if merchant.save
-        flash[:status] = :success
-        flash[:result_text] = "Logged in as new user #{merchant.name}"
+        # flash[:status] = :success
+        # flash[:result_text] = "Logged in as new user #{merchant.name}"
       else
-        flash[:status] = :failure
-        flash[:result_text] = "Could not create new user account: #{merchant.errors.messages}"
-        flash.now[:messages] = merchant.errors.messages
+        # flash[:status] = :failure
+        # flash[:result_text] = "Could not create new user account: #{merchant.errors.messages}"
+        # flash.now[:messages] = merchant.errors.messages
         redirect_to root_path
         return
       end
@@ -30,8 +30,8 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:status] = :success
-    flash[:result_text] = "Successfully logged out"
+    # flash[:status] = :success
+    # flash[:result_text] = "Successfully logged out"
 
     redirect_to root_path
   end
