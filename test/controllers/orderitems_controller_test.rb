995236a4452d681db157636709eca6c69cce0789
Alice Hsiao <alice.hsiao87@gmail.com>
2018-10-19 16:20:49 -0700

    Created orderitems controller

--- test/controllers/orderitems_controller_test.rb
+++ test/controllers/orderitems_controller_test.rb
@@ -1,4 +1,18 @@
 require "test_helper"
 
-describe OrdersController do
+describe OrderitemsController do
+  describe "create" do
+    it "should create new guest if no one is logged on" do
+
+    end
+
+    it "should log in the guest" do
+    end
+
+    it "should create a new order" do
+    end
+
+    it "should create a new orderitem" do
+    end
+  end
 end
