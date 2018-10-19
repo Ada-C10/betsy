7bb4791ced5c476d7e4e29181627e59876057fb1
Alice Hsiao <alice.hsiao87@gmail.com>
2018-10-19 16:34:41 -0700

    Removed Guest Model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,6 @@
 class Order < ApplicationRecord
   STATUSES = %w(pending paid complete cancelled)
   has_many :orderitems
-  belongs_to :guests
   has_many :products, through: :orderitems
 
   validates :status, presence: true, inclusion: { in: STATUSES }
--- test/models/guest_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe Guest do
-  let(:guest) { Guest.new }
-
-  it "must be valid" do
-    value(guest).must_be :valid?
-  end
-end
