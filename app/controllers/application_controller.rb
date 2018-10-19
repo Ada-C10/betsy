056dc6ba1f2020e60f9465b54c20e3fb4b51d55f
Katrina Aganon <katrina.aganon@gmail.com>
2018-10-18 23:15:25 -0700

    add views for categories, update nav bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :find_home_category
+
+  def find_home_category
+    @philippines = Category.find_by(name: 'philippines' )
+
+  end
+
 end
