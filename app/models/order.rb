f8fd9f32b59bc46e42f2a0ed3ab3c519a30f74db
Shelan <shelandebesai@gmail.com>
2018-10-20 15:22:33 -0700

    validations for model tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,18 +1,9 @@
 class Order < ApplicationRecord
   STATUSES = %w(pending paid complete cancelled)
   has_many :orderitems
+  belongs_to :guests
   has_many :products, through: :orderitems
 
   validates :status, presence: true, inclusion: { in: STATUSES }
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :address, presence: true
-  validates :cc_num, presence: true
-<<<<<<< HEAD
-  validates :cvv, presence: true,
-=======
-  validates :cvv, presence: true
->>>>>>> 2a1bc91b623fab2bb85dfcdbfa311fd9d891b436
-  validates :exp_date, presence: true
-  validates :zip, presence: true
+
 end
