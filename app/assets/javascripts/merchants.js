82b36b37bf2456c70663004f07966375e609493d
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-21 01:16:26 -0700

    Added cards for products on product#index, product#show, and merchant_manage_products

--- app/assets/javascripts/merchants.js
+++ app/assets/javascripts/merchants.js
@@ -1,2 +1,24 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(() => {
+  let url = location.href.replace(/\/$/, "");
+
+  if (location.hash) {
+    const hash = url.split("#");
+    $('#myTab a[href="#'+hash[1]+'"]').tab("show");
+    url = location.href.replace(/\/#/, "#");
+    history.replaceState(null, null, url);
+    setTimeout(() => {
+      $(window).scrollTop(0);
+    }, 400);
+  }
+
+  $('a[data-toggle="tab"]').on("click", function() {
+    let newUrl;
+    const hash = $(this).attr("href");
+    newUrl = url.split("#")[0] + hash;
+    newUrl += "/";
+    history.replaceState(null, null, newUrl);
+  });
+});
