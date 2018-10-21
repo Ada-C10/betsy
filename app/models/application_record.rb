39f15575d9285c731f4f41da4dab4ac1fb70efcc
Alice Hsiao <alice.hsiao87@gmail.com>
2018-10-21 11:57:15 -0700

    Full text search with postgreSQL implemented

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.text_search(query)
+    if query.present?
+      return search(query)
+    else
+      return scoped
+    end
+  end
 end
