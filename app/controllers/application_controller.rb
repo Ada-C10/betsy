cd15a2f73a7834a029b12bc5f18d584229f2f8a9
Pushpa Agr <pushpaagr108@gmail.com>
2018-10-20 17:46:14 -0700

    added new route for an my_account page that is only accessible to merchants that are logged in, also added merhcants#account action, and a view template for their account, and created links to add new product or new category

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :find_home_category
+  before_action :find_logged_in_merchant
 
   private
-  
+
   def find_home_category
     all_categories = Category.all
 
@@ -11,7 +12,13 @@ class ApplicationController < ActionController::Base
     all_categories.each do |c|
       @categories << c
     end
-
   end
+
+
+
+  def find_logged_in_merchant
+    @logged_in_merchant = Merchant.find_by(id: session[:merchant_id])
+  end
+
 
 end
