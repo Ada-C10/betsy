590fcf2f5a17a7d8d34fa3e7b2e14fee465b0071
Katrina Aganon <katrina.aganon@gmail.com>
2018-10-21 16:05:05 -0700

    add validations to category and order-items

--- app/helpers/order_items_helper.rb
+++ app/helpers/order_items_helper.rb
@@ -1,2 +1,38 @@
 module OrderItemsHelper
 end
+
+
+
+
+
+
+<div class="form-group">
+  <%= f.label :image_http_address %>
+  <%= f.text_field :img_file, placeholder: "GIF, JPG, or PNG file name" class: "form-control"  %>
+</div>
+
+
+use f.number_field for capacity
+
+add placeholder img
+
+(update params in controllers to accept new changes in the forms)
+
+helper:
+ - products helper
+
+def find_image_for(product)
+  if product.img_file.blank?
+  image_tag("default.png")
+  else
+  image_tag(product.img_file)
+  end
+end
+ - find_image(product)
+ if product.image_file
+
+
+   <div class="form-group">
+     <%= f.label :image_http_address %>
+     <%= f.text_field :img_file, class: "form-control"  %>
+   </div>
