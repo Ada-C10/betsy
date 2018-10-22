c664d3b59fbfa17e28420d3d393a5d38816394d5
Alice Hsiao <alice.hsiao87@gmail.com>
2018-10-22 13:27:03 -0700

    Fixed review strong params

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,14 +3,24 @@ class CategoriesController < ApplicationController
   # Need to be logged in to create a new category
 
   def index
+    @categories = Category.all.order(:name)
   end
 
   def show
+    id = params[:id].to_i
+    @category = Category.find_by(id: id)
+    @products = @category.products
+
+    if @category.nil?
+     render "layouts/notfound", status: :not_found
+    end
   end
 
   def new
+
   end
-  
+
   def create
+    
   end
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,7 +1,55 @@
 require "test_helper"
 
 describe CategoriesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  let(:category) { categories(:africa) }
+
+  describe "index" do
+    it "suceeds when there are categories" do
+      get categories_path
+
+      must_respond_with :success
+    end
+
+    it "suceeds when there are no categories" do
+      categories.each do |category|
+        category.destroy
+      end
+
+      new_count = Category.all.count
+      expect(new_count).must_equal 0
+
+      get categories_path
+
+      must_respond_with :success
+    end
+  end
+
+  describe "show" do
+    it "should get a category's show page" do
+      id = category.id
+
+      get category_path(id)
+
+      must_respond_with :success
+    end
+
+    it "should respond with not found if given an invalid id" do
+      id = -1
+
+      get category_path(id)
+
+      must_respond_with :notfound
+    end
+  end
+
+  describe "create" do
+    it "creates a category" do
+    end
+  end
+
+  describe "new" do
+    it "succeeds" do
+    end
+  end
+
 end
