7cb1c65728fba7a953b37a8516e4c67555a0b5e8
Anibel America <ameriabel@gmail.com>
2018-10-22 16:24:18 -0700

    adds order model validations on update action only

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,13 @@
 class Order < ApplicationRecord
   has_many :order_items
 
-  # validates_length_of :cc_num, numericality {on: :update
-  validates :cc_num, length: { is: 16, on: :update, message: "Credit Card number must be 16 digits"}
+  validates :cc_num, presence: true, length: { is: 16, message: "Credit Card number must be 16 digits" }, on: :update
+  validates :name, presence: true, on: :update
+  validates :email, presence: true, on: :update
+  validates :address, presence: true, on: :update
+  validates :zip, presence: true, length: { is: 5 }, on: :update
+  validates :cvv, presence: true, on: :update
+  validates :exp_date, presence: true, on: :update
 
 
   TAX_RATE = 0.101
