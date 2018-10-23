ace48dc823fd618d8e970f8354f46fd80230e0fb
Carly Reiter <yepodacarly@gmail.com>
2018-10-23 16:52:34 -0700

    added create to category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,17 +2,34 @@ class CategoriesController < ApplicationController
   before_action :find_category, only:[:show]
 
   def new
-    @category = Category.new 
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:success] = "Congratulations - you successfully entered a new category!"
+      redirect_to category_path(@category.id)
+    else
+      flash.now[:error] = "The data you entered was not valid.  Please try again."
+      render :new, status: :bad_request
+    end
   end
 
   def show; end
 
-  def category_params
-    return params.require(:category).permit(:name)
-  end
-
   def find_category
     @category = Category.find_by(id: params[:id])
+  end
+
+  private
+
+  def category_params
+    return params.require(:category).permit(
+      :name,
+      product_ids: []
+    )
   end
 
 end
