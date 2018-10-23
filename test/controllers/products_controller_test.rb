05cc69c465854a82f1dfd5186ceb336df1e34bff
Carly Reiter <yepodacarly@gmail.com>
2018-10-23 12:32:56 -0700

    product controller testing for show page.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -18,21 +18,47 @@ describe ProductsController do
         get products_path
         must_respond_with :success
       end
+
+      it "logged in users can access the show page for a product" do
+        product_id = Product.first.id
+        get product_path(product_id)
+        must_respond_with :success
+      end
+
+      it "logged in users cannot access the show page for a product that doesn't exist" do
+        product_id = Product.first.id + 1
+
+        get product_path(product_id)
+        must_respond_with :not_found
+      end
     end
 
   end
 
   describe "Guest users" do
     describe "access" do
-      it "logged in users can access the homepage" do
+      it "guest users can access the homepage" do
         get home_path
         must_respond_with :success
       end
 
-      it "logged in users can access the product index page" do
+      it "guest users can access the product index page" do
         products = Product.all
         get products_path
         must_respond_with :success
+      end
+
+      it "guest users can access the show page for a product" do
+        product_id = Product.first.id
+        get product_path(product_id)
+        must_respond_with :success
+      end
+
+      it "guest users cannot access the show page for a product that doesn't exist" do
+        product_id = Product.first.id + 1
+
+        get product_path(product_id)
+        must_respond_with :not_found
       end
     end
 
