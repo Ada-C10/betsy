caf6edcbb55837b5bad8b7daeb453d991f8be57f
Alice Hsiao <alice.hsiao87@gmail.com>
2018-10-22 20:03:41 -0700

    Fixed orderitem controller

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Betsy
   class Application < Rails::Application
+    config.time_zone = 'Pacific Time (US & Canada)'
   config.generators do |g|
     # Force new test files to be generated in the minitest-spec style
     g.test_framework :minitest, spec: true
