3123cecea048f486ba2d7946470ff771875aa7ae
Katie Jahanmir <katietj@gmail.com>
2018-10-23 14:38:07 -0700

    auth testing

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -5,55 +5,163 @@ describe ProductsController do
   let(:category) { categories(:africa) }
   let(:kiki) { merchants (:kiki) }
 
-  describe "index" do
-    it "succeeds when there are products" do
-      get products_path
-      must_respond_with :success
-    end
+  describe "All users " do
 
-    it "succeeds when there are no products" do
-      products.each do |product|
-        product.destroy
+    describe "index" do
+      it "succeeds when there are products" do
+        get products_path
+        must_respond_with :success
       end
 
-      new_count = Product.all.count
-      expect(new_count).must_equal 0
+      it "succeeds when there are no products" do
+        products.each do |product|
+          product.destroy
+        end
 
-      get products_path
+        new_count = Product.all.count
+        expect(new_count).must_equal 0
 
-      must_respond_with :success
+        get products_path
+
+        must_respond_with :success
+      end
     end
 
-    it "succeeds when there is a merchant id" do
-      perform_login(fred)
-      merchant_params = {
-        merchant_id: fred.id
-      }
-      get products_path, params: merchant_params
-      must_respond_with :success
-    end
-  end
+    describe "show" do
+      it "succeeds for an extant product ID" do
+        get product_path(products(:safari).id)
 
-  describe "new" do
-    it "succeeds given merchant id" do
+        must_respond_with :success
+      end
 
-      perform_login(fred)
-      get new_merchant_product_path(fred.id)
+      it "renders 404 not_found for a bogus work ID" do
+        id = -1
 
-      must_respond_with :success
+        get product_path(id)
+
+        must_respond_with 404
+      end
     end
 
-    it "does not work without a merchant id" do
-      perform_login(kiki)
-      get new_merchant_product_path(fred.id)
-      must_respond_with :not_found
-    end
-  end
+    describe "logged in users" do
 
-  describe "create" do
-    it "creates a product with valid data" do
-      perform_login(fred)
-      product_hash = {
+      before do
+        perform_login(fred)
+      end
+
+      describe "index" do
+        it "succeeds when there is a merchant id" do
+
+          merchant_params = {
+            merchant_id: fred.id
+          }
+          get products_path, params: merchant_params
+          must_respond_with :success
+        end
+      end
+
+
+
+    describe "new" do
+      it "succeeds given merchant id" do
+        get new_merchant_product_path(fred.id)
+
+        must_respond_with :success
+      end
+
+      it "does not work without a merchant id" do
+        perform_login(fred)
+        get new_merchant_product_path(kiki.id)
+        must_respond_with :not_found
+      end
+    end
+
+    describe "create" do
+      it "creates a product with valid data" do
+        product_hash = {
+          product: {
+            name: "Peru",
+            cost: 1890,
+            image_url: "image",
+            inventory: 3,
+            description: "The owls are not what they seem.",
+            active: true,
+            merchant_id: fred.id,
+            category: "South America"
+          }
+        }
+
+        old_count = Category.all.count
+        expect{
+          post products_path, params: product_hash
+        }.must_change 'Product.count', 1
+
+        new_count = Category.all.count
+        expect(new_count).must_equal (old_count + 1)
+
+        must_respond_with :redirect
+
+        expect(Product.last.name).must_equal product_hash[:product][:name]
+        expect(Product.last.cost).must_equal product_hash[:product][:cost]
+        expect(Product.last.image_url).must_equal product_hash[:product][:image_url]
+        expect(Product.last.inventory).must_equal product_hash[:product][:inventory]
+        expect(Product.last.description).must_equal product_hash[:product][:description]
+        expect(Product.last.active).must_equal product_hash[:product][:active]
+        expect(Product.last.merchant_id).must_equal product_hash[:product][:merchant_id]
+      end
+
+      it "renders bad_request and does not update the DB for bogus data" do
+        perform_login(fred)
+        product_hash = {
+          product: {
+            name: "Peru",
+            cost: 1890,
+            image_url: "image",
+            inventory: 3,
+            description: "The owls are not what they seem.",
+            active: true,
+            merchant_id: fred
+          }
+        }
+
+        product_hash[:product][:name] = nil
+
+        expect {
+          post products_path, params: product_hash
+        }.wont_change 'Product.count'
+
+        expect(flash[:status]).must_equal :failure
+        must_respond_with :bad_request
+      end
+    end
+
+    describe "edit" do
+      it "succeeds for an extant product ID" do
+        get edit_merchant_product_path(merchants(:fred),products(:fannypack).id)
+
+        must_respond_with :success
+      end
+
+      it "renders 404 not_found for a bogus work ID" do
+        products(:safari).id = -1
+
+        get edit_merchant_product_path(merchants(:fred),products(:safari).id)
+
+        must_respond_with 404
+      end
+
+      it "renders 404 not_found if current user is not merchant, i.e. the products owner" do
+        merchant = merchants(:sally)
+        product = products(:safari)
+
+        get edit_merchant_product_path(merchant, product.id)
+
+        must_respond_with :not_found
+      end
+    end
+
+    describe "update" do
+      let (:product_hash)  { {
         product: {
           name: "Peru",
           cost: 1890,
@@ -61,115 +169,12 @@ describe ProductsController do
           inventory: 3,
           description: "The owls are not what they seem.",
           active: true,
-          merchant_id: fred.id,
-          category: "South America"
-         }
-       }
+          merchant_id: merchants(:fred).id
 
-      old_count = Category.all.count
-      expect{
-        post products_path, params: product_hash
-      }.must_change 'Product.count', 1
-
-      new_count = Category.all.count
-      expect(new_count).must_equal (old_count + 1)
-
-      must_respond_with :redirect
-
-      expect(Product.last.name).must_equal product_hash[:product][:name]
-      expect(Product.last.cost).must_equal product_hash[:product][:cost]
-      expect(Product.last.image_url).must_equal product_hash[:product][:image_url]
-      expect(Product.last.inventory).must_equal product_hash[:product][:inventory]
-      expect(Product.last.description).must_equal product_hash[:product][:description]
-      expect(Product.last.active).must_equal product_hash[:product][:active]
-      expect(Product.last.merchant_id).must_equal product_hash[:product][:merchant_id]
-    end
-
-    it "renders bad_request and does not update the DB for bogus data" do
-      perform_login(fred)
-      product_hash = {
-        product: {
-          name: "Peru",
-          cost: 1890,
-          image_url: "image",
-          inventory: 3,
-          description: "The owls are not what they seem.",
-          active: true,
-          merchant_id: fred
         }
       }
-
-      product_hash[:product][:name] = nil
-
-      expect {
-        post products_path, params: product_hash
-      }.wont_change 'Product.count'
-
-      expect(flash[:status]).must_equal :failure
-      must_respond_with :bad_request
-    end
-  end
-
-  describe "show" do
-    it "succeeds for an extant product ID" do
-      get product_path(products(:safari).id)
-
-      must_respond_with :success
-    end
-
-    it "renders 404 not_found for a bogus work ID" do
-      id = -1
-
-      get product_path(id)
-
-      must_respond_with 404
-    end
-  end
-
-  describe "edit" do
-    it "succeeds for an extant product ID" do
-      perform_login(fred)
-      get edit_merchant_product_path(merchants(:fred),products(:fannypack).id)
-
-      must_respond_with :success
-    end
-
-    it "renders 404 not_found for a bogus work ID" do
-      perform_login(fred)
-      products(:safari).id = -1
-
-      get edit_merchant_product_path(merchants(:fred),products(:safari).id)
-
-      must_respond_with 404
-    end
-
-    it "renders 404 not_found if current user is not merchant, i.e. the products owner" do
-      perform_login(fred)
-      merchant = merchants(:sally)
-      product = products(:safari)
-
-      get edit_merchant_product_path(merchant, product.id)
-
-      must_respond_with :not_found
-    end
-  end
-
-  describe "update" do
-    let (:product_hash)  { {
-      product: {
-        name: "Peru",
-        cost: 1890,
-        image_url: "image",
-        inventory: 3,
-        description: "The owls are not what they seem.",
-        active: true,
-        merchant_id: merchants(:fred).id
-
-      }
     }
-  }
     it "succeeds for valid data and an extant product ID" do
-      perform_login(fred)
       id = products(:fannypack).id
 
       expect{
@@ -192,7 +197,6 @@ describe ProductsController do
 
 
     it "renders bad_request for bogus data" do
-      perform_login(fred)
       product_hash[:product][:name] = nil
 
       id = products(:fannypack).id
@@ -216,7 +220,6 @@ describe ProductsController do
     end
 
     it "responds with not found for a bogus product ID" do
-      perform_login(fred)
       id = -1
 
       expect {
@@ -231,18 +234,15 @@ describe ProductsController do
   describe "status" do
 
     it "can change status from true to false" do
-
-      perform_login(fred)
       product = products(:fannypack)
 
-    expect{patch products_status_path(merchants(:fred).id,product.id)}.wont_change 'Product.count'
+      expect{patch products_status_path(merchants(:fred).id,product.id)}.wont_change 'Product.count'
 
-    must_respond_with :redirect
+      must_respond_with :redirect
 
     end
 
     it "can change status from false to true" do
-      perform_login(fred)
       product = products(:fannypack)
       product.active = false
       product.save
@@ -252,4 +252,10 @@ describe ProductsController do
 
     end
   end
+end
+end
+
+describe "guest users" do
+
+end
 end
