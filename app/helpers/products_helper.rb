4cd0ec5e431aa0ad52ec867f66215d37dc58501f
Katrina Aganon <katrina.aganon@gmail.com>
2018-10-23 15:34:03 -0700

    update more page views for adding reviews

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,22 +1,3 @@
 module ProductsHelper
 
-  # def cookie_rating
-  #   self.average_rating.to_i.times do
-  #     <i class="fa fa-cookie-bite"></i>.html_safe
-  #   end
-  # end
-
-
-  # module ApplicationHelper
-  #   def readable_date(date)
-  #     return "[unknown]" if date.nil?
-  #     return [
-  #       "<span class='date' title='#{date}'>",
-  #       time_ago_in_words(date),
-  #       " ago",
-  #       "</span>"
-  #     ].join.html_safe
-  #   end
-  # end
-
 end
