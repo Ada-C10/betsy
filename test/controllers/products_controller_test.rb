94b99ced80389e5541938152388b033bd445ada1
Alice Hsiao <alice.hsiao87@gmail.com>
2018-10-23 15:02:50 -0700

    Completed Authorization testing for product

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,63 +4,59 @@ describe ProductsController do
   let(:fred) { merchants(:fred) }
   let(:category) { categories(:africa) }
   let(:kiki) { merchants (:kiki) }
+  let(:product) { products(:fannypack) }
 
-  describe "All users " do
+  describe "index" do
+    it "succeeds when there are products" do
+      get products_path
+      must_respond_with :success
+    end
+
+    it "succeeds when there are no products" do
+      products.each do |product|
+        product.destroy
+      end
+
+      new_count = Product.all.count
+      expect(new_count).must_equal 0
+
+      get products_path
+
+      must_respond_with :success
+    end
+  end
+
+  describe "show" do
+    it "succeeds for an extant product ID" do
+      get product_path(products(:safari).id)
+
+      must_respond_with :success
+    end
+
+    it "renders 404 not_found for a bogus work ID" do
+      id = -1
+
+      get product_path(id)
+
+      must_respond_with 404
+    end
+  end
+
+  describe "Logged In Users" do
+    before do
+      perform_login(fred)
+    end
 
     describe "index" do
-      it "succeeds when there are products" do
-        get products_path
-        must_respond_with :success
-      end
+      it "succeeds when there is a merchant id" do
 
-      it "succeeds when there are no products" do
-        products.each do |product|
-          product.destroy
-        end
-
-        new_count = Product.all.count
-        expect(new_count).must_equal 0
-
-        get products_path
-
+        merchant_params = {
+          merchant_id: fred.id
+        }
+        get products_path, params: merchant_params
         must_respond_with :success
       end
     end
-
-    describe "show" do
-      it "succeeds for an extant product ID" do
-        get product_path(products(:safari).id)
-
-        must_respond_with :success
-      end
-
-      it "renders 404 not_found for a bogus work ID" do
-        id = -1
-
-        get product_path(id)
-
-        must_respond_with 404
-      end
-    end
-
-    describe "logged in users" do
-
-      before do
-        perform_login(fred)
-      end
-
-      describe "index" do
-        it "succeeds when there is a merchant id" do
-
-          merchant_params = {
-            merchant_id: fred.id
-          }
-          get products_path, params: merchant_params
-          must_respond_with :success
-        end
-      end
-
-
 
     describe "new" do
       it "succeeds given merchant id" do
@@ -170,92 +166,135 @@ describe ProductsController do
           description: "The owls are not what they seem.",
           active: true,
           merchant_id: merchants(:fred).id
-
+            }
+          }
         }
-      }
-    }
-    it "succeeds for valid data and an extant product ID" do
-      id = products(:fannypack).id
 
-      expect{
-        patch product_path(id), params: product_hash
-      }.wont_change 'Product.count'
-      must_respond_with :redirect
-      flash[:status] = :success
+      it "succeeds for valid data and an extant product ID" do
+        id = products(:fannypack).id
+
+        expect{
+          patch product_path(id), params: product_hash
+        }.wont_change 'Product.count'
+        must_respond_with :redirect
+        flash[:status] = :success
 
 
-      product = Product.find_by(id: id)
-      expect(product.name).must_equal product_hash[:product][:name]
-      expect(product.cost).must_equal product_hash[:product][:cost]
-      expect(product.image_url).must_equal product_hash[:product][:image_url]
-      expect(product.inventory).must_equal product_hash[:product][:inventory]
-      expect(product.description).must_equal product_hash[:product][:description]
-      expect(product.active).must_equal product_hash[:product][:active]
-      expect(product.merchant_id).must_equal product_hash[:product][:merchant_id]
+        product = Product.find_by(id: id)
+        expect(product.name).must_equal product_hash[:product][:name]
+        expect(product.cost).must_equal product_hash[:product][:cost]
+        expect(product.image_url).must_equal product_hash[:product][:image_url]
+        expect(product.inventory).must_equal product_hash[:product][:inventory]
+        expect(product.description).must_equal product_hash[:product][:description]
+        expect(product.active).must_equal product_hash[:product][:active]
+        expect(product.merchant_id).must_equal product_hash[:product][:merchant_id]
+      end
 
+
+      it "renders bad_request for bogus data" do
+        product_hash[:product][:name] = nil
+
+        id = products(:fannypack).id
+        old_fannypack = products(:fannypack)
+
+        expect {
+          patch product_path(id), params: product_hash
+        }.wont_change 'Product.count'
+
+        new_fannypack = Product.find(id)
+
+        must_respond_with :bad_request
+        expect(old_fannypack.name).must_equal new_fannypack.name
+        expect(old_fannypack.cost).must_equal new_fannypack.cost
+        expect(old_fannypack.image_url).must_equal new_fannypack.image_url
+        expect(old_fannypack.inventory).must_equal new_fannypack.inventory
+        expect(old_fannypack.description).must_equal new_fannypack.description
+        expect(old_fannypack.active).must_equal new_fannypack.active
+        expect(old_fannypack.merchant_id).must_equal new_fannypack.merchant_id
+      end
+
+      it "responds with not found for a bogus product ID" do
+        id = -1
+
+        expect {
+          patch product_path(id), params: product_hash
+        }.wont_change 'Product.count'
+
+        must_respond_with :not_found
+      end
     end
 
+    describe "status" do
+      it "can change status from true to false" do
+        product = products(:fannypack)
 
-    it "renders bad_request for bogus data" do
-      product_hash[:product][:name] = nil
+        expect{patch products_status_path(merchants(:fred).id,product.id)}.wont_change 'Product.count'
 
-      id = products(:fannypack).id
-      old_fannypack = products(:fannypack)
+        must_respond_with :redirect
+      end
 
-      expect {
-        patch product_path(id), params: product_hash
-      }.wont_change 'Product.count'
+      it "can change status from false to true" do
+        product = products(:fannypack)
+        product.active = false
+        product.save
+        expect{patch products_status_path(merchants(:fred).id,product.id)}.wont_change 'Product.count'
 
-      new_fannypack = Product.find(id)
-
-      must_respond_with :bad_request
-      expect(old_fannypack.name).must_equal new_fannypack.name
-      expect(old_fannypack.cost).must_equal new_fannypack.cost
-      expect(old_fannypack.image_url).must_equal new_fannypack.image_url
-      expect(old_fannypack.inventory).must_equal new_fannypack.inventory
-      expect(old_fannypack.description).must_equal new_fannypack.description
-      expect(old_fannypack.active).must_equal new_fannypack.active
-      expect(old_fannypack.merchant_id).must_equal new_fannypack.merchant_id
-
-    end
-
-    it "responds with not found for a bogus product ID" do
-      id = -1
-
-      expect {
-        patch product_path(id), params: product_hash
-      }.wont_change 'Product.count'
-
-      must_respond_with :not_found
-
-    end
-  end
-
-  describe "status" do
-
-    it "can change status from true to false" do
-      product = products(:fannypack)
-
-      expect{patch products_status_path(merchants(:fred).id,product.id)}.wont_change 'Product.count'
-
-      must_respond_with :redirect
-
-    end
-
-    it "can change status from false to true" do
-      product = products(:fannypack)
-      product.active = false
-      product.save
-      expect{patch products_status_path(merchants(:fred).id,product.id)}.wont_change 'Product.count'
-
-      must_respond_with :redirect
-
+        must_respond_with :redirect
+      end
     end
   end
-end
-end
 
-describe "guest users" do
+  describe "guest users" do
+    describe "index" do
+      it "succeeds when there is no merchant id" do
+        get products_path
+        must_respond_with :success
+      end
+    end
 
-end
+    describe "new" do
+      it "cannot access product/new path" do
+        get new_merchant_product_path(product.id)
+
+        must_respond_with :redirect
+        must_redirect_to root_path
+      end
+    end
+
+    describe "create" do
+      it "cannot create a new product" do
+        post products_path
+
+        must_respond_with :redirect
+        must_redirect_to root_path
+      end
+    end
+
+    describe "update" do
+      it "cannot update a new product" do
+        patch merchant_product_path(fred.id, product.id)
+
+        must_respond_with :redirect
+        must_redirect_to root_path
+      end
+    end
+
+    describe "edit" do
+      it "cannot edit a new product" do
+        get edit_merchant_product_path(fred.id, product.id)
+
+        must_respond_with :redirect
+        must_redirect_to root_path
+      end
+    end
+
+    describe "status" do
+      it "cannot change the status of a product" do
+        patch products_status_path(fred.id, product.id)
+
+        must_respond_with :redirect
+        must_redirect_to root_path
+      end
+    end
+  end
 end
