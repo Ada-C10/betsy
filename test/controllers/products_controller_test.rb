356bf39c01c69dff594ea2cf35b2d9e171891e8b
Carly Reiter <yepodacarly@gmail.com>
2018-10-23 11:41:40 -0700

    beginning product controller testing.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,20 +1,32 @@
 require "test_helper"
 
 describe ProductsController do
-  describe "homepage" do
-    it "can access the homepage" do
-      get home_path
-      must_respond_with :success
-    end
-  end
-
-  describe "index" do
-    it "must be valid" do
-      products = Product.all
-      get products_path
-      must_respond_with :success
+  describe "Logged in users" do
+    
+    before do
+      perform_login(merchants(:bake_off))
     end
 
+    describe "homepage" do
+      it "can access the homepage" do
+        get home_path
+        must_respond_with :success
+      end
+    end
+
+    describe "index" do
+      it "must be valid" do
+        products = Product.all
+        get products_path
+        must_respond_with :success
+      end
+    end
 
   end
+
+  describe "Guest users" do
+
+  end
+
+
 end
