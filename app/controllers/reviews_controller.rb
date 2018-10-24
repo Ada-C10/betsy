25a09c6d1ec64164dbc124f8543c68cefb1e593a
Katrina Aganon <katrina.aganon@gmail.com>
2018-10-24 11:24:37 -0700

    update review to limit reviews for merchant's own products

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,14 +5,24 @@ class ReviewsController < ApplicationController
     product = Product.find_by(id: params[:id])
     review.product = product
 
-    if review.save
-      flash[:status] = :success
-      flash[:result_text] = "Thanks for your review!"
+    # PUT THIS IN A MODEL
+    merchant_products = Product.all.where(merchant_id: @logged_in_merchant.id)
+    product_ids = merchant_products.map { |p| p.id }
 
-      redirect_to product_path(product.id)
+    if product_ids.include?(@logged_in_merchant.id)
+      if review.save
+        flash[:status] = :success
+        flash[:result_text] = "Thanks for your review!"
+
+        redirect_to product_path(product.id)
+      else
+        flash[:status] = :failure
+        flash[:result_text] = "There was a problem saving your review"
+        redirect_back fallback_location: root_path
+      end
     else
       flash[:status] = :failure
-      flash[:result_text] = "There was a problem saving your review"
+      flash[:result_test] = "Cannot add a review for your own product!"
       redirect_back fallback_location: root_path
     end
 
