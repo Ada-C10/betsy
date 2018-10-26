2ee86b3cf0af69e33fa2326b9cefe2edee4e86b9
Carly Reiter <yepodacarly@gmail.com>
2018-10-26 06:48:50 -0700

    initial start to review controller testing.

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,5 +1,53 @@
 require "test_helper"
+require 'pry'
 
 describe ReviewsController do
 
+  describe "Logged in merchants" do
+
+    before do
+      perform_login(merchants(:bake_off))
+    end
+
+    describe "create-ability for logged-in merchants" do
+      it "can create a review for products that aren't their own" do
+
+      end
+
+      it "cannot create a review for their own products" do
+      end
+
+      it "cannot create a review for a product that doesn't exist" do
+      end
+    end
+  end
+
+  describe "Guest users" do
+    describe "Guest users can create a review" do
+
+      it "guests can create a review for a product that exists" do
+        review_data = {
+          review: {
+            rating: 5,
+            description: "great",
+            product_id: 3
+          }
+        }
+
+        review = Review.new(review_data(:review))
+
+        expect {
+          post create_review_path(review)
+        }.must_change('Review.count', +1)
+
+
+        must_respond_with :success
+
+      end
+
+      it "guests cannot create a review for a product that doesn't exist" do
+
+      end
+    end
+  end
 end
